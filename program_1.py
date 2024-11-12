d41f64d145cdbc50be34a3b36e83a4643dff68f0
MJDeC <mjdecaro@students.unwsp.edu>
2024-11-12 16:01:45 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,1 +1,12 @@
+import tkinter as tk  
 
+def gui():  
+    window=tk.Tk()  
+    window.title("Favorite Quote")  
+  
+    tk.Label(window,text='"Innovation distinguishes between a leader and a follower."\n(said by Steve Jobs)',padx=10000,pady=200,font=("Comfortaa", 41),justify="center").pack()  
+
+    window.mainloop()  
+
+if __name__ == "__main__":  
+    gui()
